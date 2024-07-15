95fbb0e20db3c2c64f1bf55123035ef8cb591838
Rendrum <iuragutan2000@mail.ru>
2024-07-15 23:22:02 +0300

    index rework

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
-
 import App from './App.jsx';
+import React from 'react';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
