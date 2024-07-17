18fa28100f89b16a824f098185b71c0fd3458936
Rendrum <iuragutan2000@mail.ru>
2024-07-17 08:39:17 +0300

    add player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Player from "./components/Player"
 
-function App() {
-  
-
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+export default function App() {
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
+      </ol>
+      GAME BOARD
+    </div>
+    LOG
+  </main>
 }
-
-export default App
